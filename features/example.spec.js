8ba7fd215fc0579ec9f15adc9bd3de146d09f66f
nateekar <nateekar@adobe.com>
2023-06-21 00:58:39 -0700

    update folders

---
+++ features/example.spec.js
@@ -0,0 +1,15 @@
+module.exports = {
+  FeatureName: 'Hello Block',
+  features: [
+    {
+      tcid: '0',
+      name: '@Hello ',
+      path: '/drafts/hello-test',
+      data: {
+        title: 'My Hello block test',
+        otherText: 'World',
+      },
+      tags: '@smoke @regression @hello,',
+    },
+  ],
+};
